f0eb0c5175db8ef30c5cf19420572496266ef1c3
Raildoc1 <Kirill1056@list.ru>
2021-07-27 00:23:11 +0700

    Grid

---
+++ Assets/_Project/Scripts/Grid/Grid.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.EditorUtilities;
+using UniRx;
+using UnityEngine;
+
+namespace SnakeGame.Grid
+{
+    public class Grid : MonoBehaviour
+    {
+        [SerializeField] private int _width = 10;
+        [SerializeField] private int _height = 10;
+
+        public IReadOnlyReactiveCollection<GridObject> Objects => _objects;
+        private ReactiveCollection<GridObject> _objects = new ReactiveCollection<GridObject>();
+
+        public void MoveOnCell(Vector2Int cell)
+        {
+            foreach (var @object in Objects)
+            {
+                if (@object.Position.Equals(cell))
+                {
+                    // TODO: Interact
+                }
+            }
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            var oldColor = Gizmos.color;
+            Gizmos.color = Color.yellow;
+
+            for (int i = 0; i <= _width; i++)
+            {
+                Gizmos.DrawLine(transform.position + i * Vector3.right, transform.position + _height * Vector3.forward + i * Vector3.right);
+            }
+            
+            for (int i = 0; i <= _height; i++)
+            {
+                Gizmos.DrawLine(transform.position + i * Vector3.forward, transform.position + _width * Vector3.right + i * Vector3.forward);
+            }
+            
+            Gizmos.color = oldColor;
+        }
+#endif
+        
+    }
+}
+
---
+++ Assets/_Project/Scripts/Grid/GridObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SnakeGame.Grid
+{
+    public class GridObject
+    {
+        private readonly Vector2Int _position;
+
+        public Vector2Int Position => _position;
+        
+        public GridObject(Vector2Int position)
+        {
+            _position = position;
+        }
+    }
+}
